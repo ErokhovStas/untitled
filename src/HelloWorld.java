43120d6e14af9bfabb7556df1bbc89be3858d2fa
megavlad <megavladislav3@gmail.com>
2021-02-01 14:59:24 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author StasErokhov
+ */
+/**
+ * @author StasErokhov
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Bye world...");
+    }
+}
